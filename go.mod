ca04fc8133481efba43336f869ceb28628fb8ea4
Don Mennerich <don.mennerich@nyu.edu>
2021-08-30 16:55:07 -0400

    addding go module, vertical layout, light theme

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/dmnyu/aspace-bulk-exporter
+
+go 1.16
+
+require (
+	fyne.io/fyne/v2 v2.0.4
+	github.com/nyudlts/go-aspace v0.3.7
+	gopkg.in/xmlpath.v1 v1.0.0-20140413065638-a146725ea6e7 // indirect
+	gopkg.in/yaml.v2 v2.4.0
+	launchpad.net/gocheck v0.0.0-20140225173054-000000000087 // indirect
+	launchpad.net/xmlpath v0.0.0-20130614043138-000000000004 // indirect
+)
--- main/main.go
+++ main/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
-	abe "github.com/dmnyu/aspace-bulk-exporter"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/theme"
 	"image/color"
 )
 
@@ -21,6 +23,13 @@ import (
 func main() {
 	a := app.New()
 	w := a.NewWindow("ASpace Bulk Exporter")
-	w.SetContent(canvas.NewText(fmt.Sprintf("ASpace Bulk Exporter, %v", abe.AppVersion), color.Black))
+	w.Resize(fyne.Size{320,240 })
+	configLoad := canvas.NewText("Place Holder for Config load", color.Black)
+	repoSelect := canvas.NewText("Place Holder for Repo Select", color.Black)
+	options := canvas.NewText("Place Holder for Options", color.Black)
+	terminal := canvas.NewText("Place Holder for log output", color.Black)
+	c := container.New(layout.NewVBoxLayout(), configLoad, repoSelect, options, terminal)
+	w.SetContent(c)
+	fyne.CurrentApp().Settings().SetTheme(theme.LightTheme())
 	w.ShowAndRun()
 }
